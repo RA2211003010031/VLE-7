9ba0cdb895d4d2b26889188c4014230202056fa9
Wiiwake3101 <vm4512@srmist.edu.in>
2025-10-07 09:58:22 +0530

    Added a line in PetClinicApplication.java file

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -30,6 +30,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring PetClinic :: vm4512_BLE");
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
